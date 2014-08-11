46d3704ac8a11c6fe058a75b3f11d125fe5064db
viggibear <armstrong.vignesh62@gmail.com>
2014-08-11 08:53:03 +0000

    first commit

---
+++ bookmark/views.py
@@ -0,0 +1,119 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+# Import the Category model
+from bookmark.models import Category
+from bookmark.models import Page
+from bookmark.forms import PageForm
+
+def decode_url(url):
+    return url.replace(' ', '_')
+  
+def index(request):
+    # Obtain the context from the HTTP request.
+    context = RequestContext(request)
+
+    # Query for categories - add the list to our context dictionary.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    # The following two lines are new.
+    # We loop through each category returned, and create a URL attribute.
+    # This attribute stores an encoded URL (e.g. spaces replaced with underscores).
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
+
+    # Render the response and return to the client.
+    return render_to_response('bookmark/index.html', context_dict, context)
+  
+  
+def category(request, category_name_url):
+    # Request our context from the request passed to us.
+    context = RequestContext(request)
+
+    # Change underscores in the category name to spaces.
+    # URLs don't handle spaces well, so we encode them as underscores.
+    # We can then simply replace the underscores with spaces again to get the name.
+    category_name = category_name_url.replace('_', ' ')
+
+    # Create a context dictionary which we can pass to the template rendering engine.
+    # We start by containing the name of the category passed by the user.
+    context_dict = {'category_name': category_name, 'category_name_url': category_name_url}
+
+    try:
+        # Can we find a category with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        # So the .get() method returns one model instance or raises an exception.
+        category = Category.objects.get(name=category_name)
+
+        # Retrieve all of the associated pages.
+        # Note that filter returns >= 1 model instance.
+        pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+        # We also add the category object from the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category.
+        # Don't do anything - the template displays the "no category" message for us.
+        pass
+
+    # Go render the response and return it to the client.
+    return render_to_response('bookmark/category.html', context_dict, context)
+  
+from bookmark.forms import CategoryForm
+
+def add_category(request):
+    # Get the context from the request.
+    context = RequestContext(request)
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render_to_response('bookmark/add_category.html', {'form': form}, context)
+
+def add_page(request, category_name_url):
+    context = RequestContext(request) #request context
+    category_name = category_name_url.replace('_', ' ') #get category name to show on webpage instead of '_'
+    try:
+        cat_exists = Category.objects.get(name=category_name) #if cat_exists, get the name of the category
+    except Category.DoesNotExist:
+        cat_exists = False #if it does not exist, make cat_exists into false to display on the website
+    if request.method == 'POST':
+        form = PageForm(request.POST) 
+        if form.is_valid():
+            new_page = form.save(commit=False) #don't commit first
+            if cat_exists: #if the category even exists
+                new_page.category_id = cat_exists.id #set the category_id of newpage to the id of the category requested in the URL
+                new_page.views = 0 #set views to 0
+            else: #category does not exist  
+                return index(request) #bring me to /bookmark/
+            new_page.save() #then save the form
+            return category(request, category_name_url) #now return the main page /bookmark//bookmark/category/<category_name_url>/
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+        
+    return render_to_response('bookmark/add_page.html', {'form': form, 'category_name_url': category_name_url, 'category_name':category_name, 'category_exists':cat_exists}, context)
